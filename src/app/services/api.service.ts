abb6f837141e4c2667bb70d1bb4d7f1ae11cd08f
Vin Wahome <vinwahome@gmail.com>
2025-01-15 10:10:12 +0300

    Create theservice to handle API calss

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8000/api/fruits';
+
+  constructor(private http: HttpClient) { }
+
+  getFruits(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
+  getFruit(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  addFruit(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}`, data);
+  }
+
+  updateFruit(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.baseUrl}/${id}`, data);
+  }
+
+  deleteFruit(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
 }
