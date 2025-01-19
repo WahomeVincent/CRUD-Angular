a65847290368e655ae65f40f7d99ed6362a541b1
Vin Wahome <vinwahome@gmail.com>
2025-01-19 17:31:04 +0300

    Http configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ListComponent } from "./components/list/list.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [ ListComponent],
+  template: '<app-list></app-list>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
